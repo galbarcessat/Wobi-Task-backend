e4af0288fb1b6456f8a79f4c5b8ed1ef9948fa41
galbarcessat <galbarcessat1@gmail.com>
2024-05-01 00:32:55 +0300

    build v1

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-  dbURL: process.env.MONGO_URL || 'mongodb+srv://galbarcessat:galbarcessat123@cluster0.vsahcr7.mongodb.net/?retryWrites=true&w=majority',
+  dbURL: process.env.MONGO_URL || 'mongodb+srv://galbarcessat:galbarcessat@cluster0.2z7lxvl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
   dbName: process.env.DB_NAME || 'wobi_db'
 }
