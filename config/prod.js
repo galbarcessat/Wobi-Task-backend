84bf601f4586a7ff226afdec34518d84d60967e4
galbarcessat <galbarcessat1@gmail.com>
2024-04-30 17:22:49 +0300

    Wobi task backend

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+export default {
+  dbURL: process.env.MONGO_URL || 'mongodb+srv://galbarcessat:galbarcessat123@cluster0.vsahcr7.mongodb.net/?retryWrites=true&w=majority',
+  dbName: process.env.DB_NAME || 'wobi_db'
+}
---
+++ server.js
@@ -0,0 +1,46 @@
+import http from 'http'
+import path from 'path'
+import cors from 'cors'
+import express from 'express'
+import cookieParser from 'cookie-parser'
+
+const app = express()
+const server = http.createServer(app)
+
+// Express App Config
+app.use(cookieParser())
+app.use(express.json({ limit: '50mb' }))
+// app.use(express.json())
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve('public')))
+} else {
+    const corsOptions = {
+        origin: [
+            'http://127.0.0.1:3000',
+            'http://localhost:3000',
+            'http://127.0.0.1:5173',
+            'http://localhost:5173'
+        ],
+        credentials: true
+    }
+    app.use(cors(corsOptions))
+}
+
+import { authRoutes } from './api/auth/auth.routes.js'
+import { userRoutes } from './api/user/user.routes.js'
+import { logger } from './services/logger.service.js'
+
+// routes
+app.use('/api/auth', authRoutes)
+app.use('/api/user', userRoutes)
+
+app.get('/**', (req, res) => {
+    res.sendFile(path.resolve('public/index.html'))
+})
+
+const port = process.env.PORT || 3030
+server.listen(port, () => {
+    logger.info('Server is running on port: ' + port)
+})
