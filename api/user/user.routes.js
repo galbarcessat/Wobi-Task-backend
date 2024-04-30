4f91c06cbafc0c7f03dc70a249878ffb1b74db76
galbarcessat <galbarcessat1@gmail.com>
2024-04-30 23:58:21 +0300

    Code clean up

--- api/user/user.routes.js
+++ api/user/user.routes.js
@@ -8,6 +8,5 @@ router.get('/', getUsers)
 router.get('/:id', getUser)
 router.put('/users', updateUsers)
 router.put('/:id', updateUser)
-router.delete('/:id', requireAuth, requireAdmin, deleteUser)
 
 export const userRoutes = router
